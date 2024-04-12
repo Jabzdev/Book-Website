b9d1ed7ad9c62fbd2d3e89e06c73893a87c24d7c
Abdul-Jabar <128983487+abduljabarcodes@users.noreply.github.com>
2024-04-12 23:12:45 +0300

    6th commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,14 @@ let swiperTestimonial = new Swiper('.testimonial-swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+  const scrollUp = document.querySelector('#scroll-up')
 
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+            : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
